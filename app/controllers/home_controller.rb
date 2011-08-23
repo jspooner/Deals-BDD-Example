1d39294461bca2001c7c6765b507403ad833744b
Jonathan Spooner <jspooner@gmail.com>
2011-08-22 18:52:36 -0700

    Added deals to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
+    @deals = Deal.where("Date(start_date) <= ? AND Date(end_date) >= ?", Date.today.to_date, Date.today.to_date)
   end
 end
